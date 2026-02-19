80414a7a4c0241bf39f59b3fd21ca260cfd13e4f
TheAbyssSage <sage.stockmans@pm.me>
2026-02-19 22:02:33 +0100

    Connecting the routes for the experience pages shows

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,12 +6,15 @@ import { Home } from './pages/home/home';
 import { About } from './pages/about/about';
 import { ProjectsComponent } from './pages/projects/projects';
 import { Contact } from './pages/contact/contact';
+import { ExperienceComponent } from './pages/experience/experience';
+
 
 export const routes: Routes = [
   { path: '', component: Home, pathMatch: 'full' },
   { path: 'about', component: About },
   { path: 'projects', component: ProjectsComponent },
   { path: 'contact', component: Contact },
+  { path: 'experience', component: ExperienceComponent },
   { path: 'privacy', component: Privacy },
   { path: 'terms', component: Terms },
   { path: 'imprint', component: Imprint },
