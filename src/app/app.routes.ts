2d892d3121adeddfc4ab17b31e5bc8370a367ad0
TheAbyssSage <sage.stockmans@pm.me>
2026-02-23 09:26:04 +0100

    Corrected routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
-import { Privacy } from './components/privacy/privacy';
-import { Terms } from './components/terms/terms';
-import { Imprint } from './components/imprint/imprint';
+import { PrivacyComponent } from './components/privacy/privacy';
+import { TermsComponent } from './components/terms/terms';
+import { ImprintComponent } from './components/imprint/imprint';
 import { Home } from './pages/home/home';
 import { About } from './pages/about/about';
 import { ProjectsComponent } from './pages/projects/projects';
@@ -15,8 +15,8 @@ export const routes: Routes = [
   { path: 'projects', component: ProjectsComponent },
   { path: 'contact', component: Contact },
   { path: 'experience', component: ExperienceComponent },
-  { path: 'privacy', component: Privacy },
-  { path: 'terms', component: Terms },
-  { path: 'imprint', component: Imprint },
+  { path: 'privacy', component: PrivacyComponent },
+  { path: 'terms', component: TermsComponent },
+  { path: 'imprint', component: ImprintComponent },
   { path: '**', redirectTo: '' },
 ];
